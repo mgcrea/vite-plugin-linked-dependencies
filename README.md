bde558469a1a400f26e018b32a385e114e08581f
Olivier Louvignes <olivier@mgcrea.io>
2021-11-28 18:45:11 +0100

    feat(git): initial release

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
-# Create TypeScript Library
+# vite-plugin-linked-dependencies
 
-Use this project to quickly bootstrap a TypeScript library project.
+<p align="center">
+  <a href="https://www.npmjs.com/package/@mgcrea/vite-plugin-linked-dependencies">
+    <img src="https://img.shields.io/npm/v/@mgcrea/vite-plugin-linked-dependencies.svg?style=for-the-badge" alt="npm version" />
+  </a>
+  <!-- <a href="https://www.npmjs.com/package/@mgcrea/vite-plugin-linked-dependencies">
+    <img src="https://img.shields.io/npm/dt/@mgcrea/vite-plugin-linked-dependencies.svg?style=for-the-badge" alt="npm total downloads" />
+  </a> -->
+  <a href="https://www.npmjs.com/package/@mgcrea/vite-plugin-linked-dependencies">
+    <img src="https://img.shields.io/npm/dm/@mgcrea/vite-plugin-linked-dependencies.svg?style=for-the-badge" alt="npm monthly downloads" />
+  </a>
+  <a href="https://www.npmjs.com/package/@mgcrea/vite-plugin-linked-dependencies">
+    <img src="https://img.shields.io/npm/l/@mgcrea/vite-plugin-linked-dependencies.svg?style=for-the-badge" alt="npm license" />
+  </a>
+  <a href="https://github.com/mgcrea/vite-plugin-linked-dependencies/actions/workflows/main.yml">
+    <img src="https://img.shields.io/github/workflow/status/mgcrea/vite-plugin-linked-dependencies/main?style=for-the-badge" alt="github main workflow" />
+  </a>
+</p>
 
-## Usage
+## Features
 
-```bash
-# Clone the project
-git clone -o source git@github.com:mgcrea/create-typescript-lib.git my-new-library; cd $_
-# Reset git history
-rm -rf .git; git init
-# Create first commit
-git add --all .; git ci -am "feat(git): bootstrap from github:mgcrea/create-typescript-lib"
+- Automatically hoist your linked dependencies' peer dependencies as local aliases inside your local `node_modules`
+  folder.
+
+## Install
+
+```sh
+npm install @mgcrea/vite-plugin-linked-dependencies
+```
+
+## Quickstart
+
+```tsx
+// vite.config.ts
+import linkedDependenciesPlugin from '@mgcrea/vite-plugin-linked-dependencies';
+
+export default defineConfig({
+  plugins: [linkedDependenciesPlugin()],
+});
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,59 @@
-export const foo = 'bar';
+import { Dirent, PathLike } from 'fs';
+import { readdir, readFile } from 'fs/promises';
+import { resolve } from 'path';
+
+type ListSymlinksOptions = {
+  depth?: number;
+  filter?: (item: Dirent) => boolean;
+};
+
+const listSymlinks = async (directory: PathLike, { depth = 0, filter }: ListSymlinksOptions) => {
+  const list = await readdir(directory, { withFileTypes: true });
+  const symlinks = list.filter((item) => item.isSymbolicLink()).map((item) => item.name);
+  if (depth <= 0) {
+    return symlinks;
+  }
+  const subFolders = list.filter((item) => item.isDirectory() && (filter ? filter(item) : true));
+  for (const folder of subFolders) {
+    const folderSymlinks = await listSymlinks(`${directory}/${folder.name}`, { depth: depth - 1 });
+    symlinks.push(...folderSymlinks.map((item) => `${folder.name}/${item}`));
+  }
+  return symlinks;
+};
+
+export default async function linkedPackagesPlugin(root = process.cwd()) {
+  const modulesDirectory = resolve(root, 'node_modules');
+  const linkedDependencies = await listSymlinks(modulesDirectory, {
+    depth: 1,
+    filter: (item) => item.name.startsWith('@'),
+  });
+  const linkedPeerDependencies: string[] = [];
+  for (const linkedDependency of linkedDependencies) {
+    const packageContents = await readFile(resolve(modulesDirectory, linkedDependency, 'package.json'), 'utf-8');
+    const packageJson = JSON.parse(packageContents);
+    if (packageJson.peerDependencies) {
+      Object.keys(packageJson.peerDependencies).forEach((name) => {
+        if (!linkedPeerDependencies.includes(name)) {
+          linkedPeerDependencies.push(name);
+        }
+      });
+    }
+  }
+  return {
+    name: 'linked-packages',
+    config(config: PartialViteConfig) {
+      config.resolve.alias.push(
+        ...linkedPeerDependencies.map((pkgName) => ({
+          find: pkgName,
+          replacement: resolve(modulesDirectory, pkgName),
+        }))
+      );
+    },
+  };
+}
+
+type PartialViteConfig = {
+  resolve: {
+    alias: { find: string; replacement: string }[];
+  };
+};
